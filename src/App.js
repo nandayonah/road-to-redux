f8c4430eb93bb2a39b3407f781520f0e28de4e9f
Fadli Hidayatullah <44917664+nandayonah@users.noreply.github.com>
2020-11-18 16:44:47 +0700

    Counter Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 1
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState((state) => ({ count: state.count + 1 }));
+  }
+
+  onDecrement() {
+    this.setState((state) => ({ count: state.count - 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+      </div>
+    );
+  }
 }
+
+export default Counter;
